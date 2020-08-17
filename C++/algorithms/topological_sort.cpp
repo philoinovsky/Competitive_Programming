a90848b100f1616c8a86506b66576c64719ef117
philoinovsky <1129410550@qq.com>
2020-08-18 02:04:09 +0800

    update

--- C++/algorithms/topological_sort.cpp
+++ C++/algorithms/topological_sort.cpp
@@ -1,34 +1,37 @@
-class Solution {
-public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> degree(numCourses, 0);
-        queue<int> zeroDegree;
-        vector<int> topoOrder;
-        for(int i = 0; i<prerequisites.size(); i++){
-            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            degree[prerequisites[i][0]]++;
-        }
-        for(int i = 0; i<numCourses; i++){
-            if(degree[i] == 0){
-                zeroDegree.push(i);
-                topoOrder.push_back(i);
-            }
-        }
-        while(!zeroDegree.empty()){
-            int node = zeroDegree.front();
-            zeroDegree.pop();
-            for(int connectedNode: graph[node]){
-                degree[connectedNode]--;
-                if(degree[connectedNode] == 0){
-                    zeroDegree.push(connectedNode);
-                    topoOrder.push_back(connectedNode);
-                }
-            }
-        }
-        if(topoOrder.size() != numCourses){
-            return vector<int>();
-        }
-        return topoOrder;
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> graph(numCourses);
+    vector<int> degree(numCourses, 0), topoOrder;
+    queue<int> zeroDegree;
+    for(int i = 0; i<prerequisites.size(); i++){
+        graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        degree[prerequisites[i][0]]++;
     }
-};
+    for(int i = 0; i<numCourses; i++){
+        if(degree[i] == 0){
+            zeroDegree.push(i);
+            topoOrder.push_back(i);
+        }
+    }
+    while(!zeroDegree.empty()){
+        int node = zeroDegree.front();
+        zeroDegree.pop();
+        for(int connectedNode: graph[node]){
+            degree[connectedNode]--;
+            if(degree[connectedNode] == 0){
+                zeroDegree.push(connectedNode);
+                topoOrder.push_back(connectedNode);
+            }
+        }
+    }
+    if(topoOrder.size() != numCourses){
+        return vector<int>();
+    }
+    return topoOrder;
+}
+
+int main(){
+    return 0;
+}
