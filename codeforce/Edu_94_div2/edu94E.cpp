9bdfd478d739e6d9200b4c08094394471a674517
philoinovsky <1129410550@qq.com>
2020-09-01 03:11:44 +0800

    update

--- codeforce/Edu_94_div2/edu94E.cpp
+++ codeforce/Edu_94_div2/edu94E.cpp
@@ -15,8 +15,25 @@ typedef unsigned long long ull;
 //global variables
 const int MAXN = 5010;
 int N, A[MAXN];
+int dp[MAXN][MAXN];
 
 //-------------function-starts---------------------
+int calc(int pos, int x){
+    int &res = dp[pos][x];
+    if(res != -1) return res;
+    if(pos == N) return res = 0;
+    res = 1 + calc(pos+1,N);
+    res = min(res,calc(pos+1,pos)+A[pos]);
+    if (x != N) {
+        if (A[x] >= A[pos]){
+            res = min(res, calc(pos+1,pos));
+        } else {
+            res = min(res, calc(pos + 1, pos) + A[pos] - a[x]);
+            res = min(res, 1 + calc(pos + 1, x));
+        }
+    }
+    return res;
+}
 //-------------function-ends-----------------------
 
 void solve(){
@@ -28,6 +45,7 @@ void solve(){
 int main(){
     cin >> N;
     rep(i,N) cin >> A[i];
-    solve();
+    memset(dp,-1,sizeof(dp));
+    cout << calc(0, N) << endl;
     return 0;
 }
