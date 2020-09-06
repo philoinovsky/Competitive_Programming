b5cc154b13c148388e74d4109c8e7aca636c02dd
philoinovsky <1129410550@qq.com>
2020-09-06 22:33:17 +0800

    update

---
+++ Python/DSU.py
@@ -0,0 +1,23 @@
+class DSU:
+    def __init__(self, N):
+        self.par = list(range(N))
+        self.sz = [1] * N
+
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return False
+        if self.sz[xr] < self.sz[yr]:
+            xr, yr = yr, xr
+        self.par[yr] = xr
+        self.sz[xr] += self.sz[yr]
+        self.sz[yr] = self.sz[xr]
+        return True
+
+    def size(self, x):
+        return self.sz[self.find(x)]
--- leetcode/lc_template.cpp
+++ leetcode/lc_template.cpp
@@ -25,27 +25,60 @@ template<typename T> void print(const T& t) { //print(vector);
 //-------------function-starts---------------------
 class Solution {
 public:
-    int findLengthOfShortestSubarray(vector<int>& arr) {
-        arr.push_back(0x7fffffff);
-        int N = arr.size();
-        int left = N, right = -1;
-        for(int i = 1; i < N; i++){
-            if(arr[i] < arr[i-1]){
-                left = min(left,i);
-                right = max(right,i);
+    pair<int,int> traverse(vector<vector<pair<int,int>>>& graph, int type, int N){
+        vector<bool> visited(N);
+        visited[0] = true;
+        deque<pair<int,int>> edgepool;
+        for(auto e: graph[0]){
+            if(e.second == 3){
+                edgepool.push_front(e);
+            } else if (e.second == type) {
+                edgepool.push_back(e);
             }
         }
-        if(left > right){
-            return 0;
-        } else {
-            vector<int> ordered;
-            for(int i = 0; i < left; i++)
-                ordered.push_back(arr[i]);
-            int MIN = N;
-            for(int i = right; i < N; i++){
-                MIN = min(MIN,(int)(i - (upper_bound(ordered.begin(),ordered.end(),arr[i])-ordered.begin())));
+        int rem = 0, cnt3 = 0;
+        while(!edgepool.empty()){
+            int node = edgepool.front().first;
+            int t = edgepool.front().second;
+            edgepool.pop_front();
+            if(visited[node]){
+                rem++;
+            } else {
+                if(t == 3)
+                    cnt3++;
+                visited[node] = true;
+                for(auto e: graph[node]){
+                    if(e.second == 3){
+                        edgepool.push_front(e);
+                    } else if (e.second == type) {
+                        edgepool.push_back(e);
+                    }
+                }
             }
-            return MIN;
+        }
+        for(int i = 0; i < N; i++){
+            if(!visited[i]){
+                return {-1,-1};
+            }
+        }
+        return {rem,cnt3};
+    }
+    int maxNumEdgesToRemove(int N, vector<vector<int>>& edges) {
+        int E = edges.size();
+        vector<vector<pair<int,int>>> graph(N);
+        int cnt[4] = {0};
+        for(int i = 0; i < E; i++){
+            graph[edges[i][1]-1].push_back({edges[i][2]-1,edges[i][0]});
+            graph[edges[i][2]-1].push_back({edges[i][1]-1,edges[i][0]});
+            cnt[edges[i][0]]++;
+        }
+        pair<int,int> res1 = traverse(graph,1,N), res2 = traverse(graph,2,N);
+        int rem1 = res1.first, rem2 = res2.first;
+        if(rem1 == -1 || rem2 == -1){
+            return -1;
+        } else {
+            // cout << cnt[3] << cnt[2] << cnt[1] << endl;
+            return rem1 + rem2 - 3*cnt[3] - cnt[2] - cnt[1] + res1.second;
         }
     }
 };
