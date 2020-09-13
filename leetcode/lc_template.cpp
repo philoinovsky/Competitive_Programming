9864ac64fe313fe69e894b458fd3fceec0c5f362
philoinovsky <1129410550@qq.com>
2020-09-13 12:13:35 +0800

    update

--- leetcode/lc_template.cpp
+++ leetcode/lc_template.cpp
@@ -25,68 +25,40 @@ template<typename T> void print(const T& t) { //print(vector);
 //-------------function-starts---------------------
 class Solution {
 public:
-    pair<int,int> traverse(vector<vector<pair<int,int>>>& graph, int type, int N){
-        vector<bool> visited(N);
-        visited[0] = true;
-        deque<pair<int,int>> edgepool;
-        for(auto e: graph[0]){
-            if(e.second == 3){
-                edgepool.push_front(e);
-            } else if (e.second == type) {
-                edgepool.push_back(e);
+    bool isTransformable(string s, string t) {
+        int cnts[10] = {0}, cntt[10] = {0};
+        int N = s.size(), M = t.size();
+        if(N != M) return false;
+        rep(i,N){
+            cnts[s[i]-'0']++;
+            cntt[t[i]-'0']++;
+        }
+        rep(i,10) if(cnts[i] != cntt[i]) return false;
+        vector<deque<vector<int>>> q(10);
+        int cnt[10] = {0};
+        repr(i,N-1){
+            vector<int> tmp(10);
+            REP(j,'0',s[i],1){
+                tmp[j-'0'] = cnt[j-'0'];
             }
+            q[s[i]-'0'].push_back(tmp);
+            cnt[s[i]-'0']++;
         }
-        int rem = 0, cnt3 = 0;
-        while(!edgepool.empty()){
-            int node = edgepool.front().first;
-            int t = edgepool.front().second;
-            edgepool.pop_front();
-            if(visited[node]){
-                rem++;
-            } else {
-                if(t == 3)
-                    cnt3++;
-                visited[node] = true;
-                for(auto e: graph[node]){
-                    if(e.second == 3){
-                        edgepool.push_front(e);
-                    } else if (e.second == type) {
-                        edgepool.push_back(e);
-                    }
-                }
+        rep(i,10) cnt[i] = 0;
+        repr(i,N-1){
+            REP(j,'0',t[i],1){
+                if(q[t[i]-'0'].front()[j-'0'] < cnt[j-'0'])
+                    return false;
             }
+            q[t[i]-'0'].pop_front();
         }
-        for(int i = 0; i < N; i++){
-            if(!visited[i]){
-                return {-1,-1};
-            }
-        }
-        return {rem,cnt3};
-    }
-    int maxNumEdgesToRemove(int N, vector<vector<int>>& edges) {
-        int E = edges.size();
-        vector<vector<pair<int,int>>> graph(N);
-        int cnt[4] = {0};
-        for(int i = 0; i < E; i++){
-            graph[edges[i][1]-1].push_back({edges[i][2]-1,edges[i][0]});
-            graph[edges[i][2]-1].push_back({edges[i][1]-1,edges[i][0]});
-            cnt[edges[i][0]]++;
-        }
-        pair<int,int> res1 = traverse(graph,1,N), res2 = traverse(graph,2,N);
-        int rem1 = res1.first, rem2 = res2.first;
-        if(rem1 == -1 || rem2 == -1){
-            return -1;
-        } else {
-            // cout << cnt[3] << cnt[2] << cnt[1] << endl;
-            return rem1 + rem2 - 3*cnt[3] - cnt[2] - cnt[1] + res1.second;
-        }
+        return true;
     }
 };
 //-------------function-ends-----------------------
 
 int main(){
     Solution sol;
-    vector<int> input = {2,2,2,1,1,1};
-    sol.findLengthOfShortestSubarray(input);
+    sol.isTransformable("12345","12435");
     return 0;
 }
