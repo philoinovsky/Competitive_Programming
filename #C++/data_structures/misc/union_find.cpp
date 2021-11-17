f7b3f4038516ecbd0c7802bd9901d079f59d43c1
philoinovsky <1129410550@qq.com>
2021-11-17 20:43:43 +0800

    add generic union find

--- #C++/data_structures/misc/union_find.cpp
+++ #C++/data_structures/misc/union_find.cpp
@@ -17,6 +17,48 @@ public:
 };
 //-------------------union-find-ends--------------------------------
 
+//-------------------union-find(generic)-starts------------------------------
+template<typename T>
+class UnionFindSet
+{
+public:
+    UnionFindSet(size_t size)
+    : m_parent(size)
+    , m_rank(size, 1)
+    , m_disjoint_cnt(size)
+    {
+        for (size_t i = 0; i < size; ++i)
+            m_parent[i] = i;
+    }
+
+    void merge(T i, T j) noexcept
+    {
+        const T& x = find(i), y = find(j);
+        if (m_rank[x] <= m_rank[y])
+            m_parent[x] = y;
+        else
+            m_parent[y] = x;
+        if (m_rank[x] == m_rank[y] && x != y)
+            m_rank[y]++;
+        if (x != y)
+            m_disjoint_cnt--;
+    }
+    
+    inline const T& find(const T& x) const
+    {
+        return (x == m_parent[x]) ? x : find(m_parent[x]);
+    }
+
+    inline size_t get_disjoint_cnt() const
+    {
+        return m_disjoint_cnt;
+    }
+private:
+    vector<T> m_parent, m_rank;
+    size_t m_disjoint_cnt;
+};
+//-------------------union-find(generic)-ends--------------------------------
+
 int main(){
     int N, M, P, X, Y;
     cin >> N >> M >> P;
